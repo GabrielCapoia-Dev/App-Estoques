ed9aa452637771512df4289021ca210ed4635b31
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2025-01-28 13:02:36 -0300

    Ajuste estetico na pagina de cadastro de pedidos

--- resources/views/pedidos/create.blade.php
+++ resources/views/pedidos/create.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Seção de seleção de filtros -->
         <div class="card mb-4">
-            <div class="card-header bg-primary text-white">
+            <div class="card-header bg-success text-white">
                 <h5>Filtrar</h5>
             </div>
             <div class="card-body">
@@ -44,14 +44,14 @@
             <!-- Seção de filtros -->
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header bg-info text-white d-flex justify-content-between align-items-center">
+                    <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
                         <h5>Lista dos itens filtrados</h5>
                         <!-- Total Geral e botão no cabeçalho -->
-                        <div class="d-flex align-items-center">
-                            <strong>Total Geral:</strong>
-                            <span id="totalGeral" class="ms-2">R$ 0,00</span>
-                            <button type="button" class="btn btn-success ms-3" id="fazerPedidoBtn">Finalizar
-                                Pedido</button>
+                        <div class="d-flex align-items-center total-container bg-white text-black rounded-3 shadow-sm p-1">
+                            <strong>Valor Total:</strong>
+                            <span id="totalGeral" class="ms-3">R$ 0,00</span>
+                            <button type="button" class="btn btn-primary ms-3" id="fazerPedidoBtn">Finalizar</button>
+                            <button type="button" class="btn ms-3" style="background-color: #11bb69; color: white" id="fazerPedidoBtn"><i class="fa-solid fa-plus"></i> Produto</button>
                         </div>
                     </div>
                     <div class="card-body">
@@ -113,7 +113,7 @@
                                                                 value="{{ $quantidadeDiferenca }}"
                                                                 id="quantidade_{{ $estoqueProduto['produto']['id'] }}"
                                                                 aria-label="Quantidade" min="0"
-                                                                style="width: auto; max-width: 50px; padding: 0.375rem 0.75rem;"
+                                                                style="width: auto; max-width: 60px; padding: 0.375rem 0.75rem;"
                                                                 onchange="atualizarTotal({{ $estoqueProduto['produto']['id'] }}, {{ $preco }})">
 
                                                             <!-- Botão de aumentar -->
@@ -135,122 +135,176 @@
                 </div>
             </div>
         </div>
+    </div>
+@endsection
 
-    @endsection
+<style>
+    /* Estilo para a div do Total Geral */
+    .total-container {
+        background-color: #ffffff;
+        /* Cor de fundo branco */
+        border-radius: 0.5rem;
+        /* Bordas arredondadas */
+        box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1);
+        /* Sombra interna */
+        padding: 12px 25px;
+        /* Aumentando o padding para ficar mais largo */
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: auto;
+    }
 
-    <style>
-        /* Remove as setas de incremento e decremento no input de número */
-        input[type="number"]::-webkit-outer-spin-button,
-        input[type="number"]::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
+    /* Estilo para o botão suave com cor verde claro */
+    .btn-soft-success {
+        background-color: #a3dbc0;
+        /* Cor verde claro */
+        color: #28a745;
+        /* Cor de texto */
+        border: 1px solid #28a745;
+        /* Borda com a mesma cor */
+        transition: background-color 0.3s, color 0.3s;
+        /* Transição suave */
+    }
+
+    .btn-soft-success:hover {
+        background-color: #8cd4a3;
+        /* Cor verde mais escura ao passar o mouse */
+        color: #2c6c2b;
+        /* Cor de texto mais escura */
+    }
+
+    /* Responsividade */
+    @media (max-width: 767px) {
+        .total-container {
+            flex-direction: column;
+            align-items: flex-start;
         }
 
-        input[type="number"] {
-            -moz-appearance: textfield;
-            /* Para Firefox */
+        .ms-2 {
+            margin-left: 0;
         }
-    </style>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Atualizar o total geral ao carregar a página
-            atualizarTotalGeral();
+        .btn-soft-success {
+            width: 100%;
+            /* Botão ocupa toda a largura */
+        }
+    }
+</style>
 
-            // Evento de mudança para o select de local
-            document.getElementById('local').addEventListener('change', function() {
-                const localId = this.value;
-                const estoqueSelect = document.getElementById('estoque');
+<style>
+    /* Remove as setas de incremento e decremento no input de número */
+    input[type="number"]::-webkit-outer-spin-button,
+    input[type="number"]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 
-                // Limpa os estoques existentes
-                estoqueSelect.innerHTML = '<option value="">Selecione</option>';
+    input[type="number"] {
+        -moz-appearance: textfield;
+        /* Para Firefox */
+    }
+</style>
 
-                if (localId) {
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Atualizar o total geral ao carregar a página
+        atualizarTotalGeral();
 
-                    const url = `/escolas/${localId}/estoques/getEstoques`;
+        // Evento de mudança para o select de local
+        document.getElementById('local').addEventListener('change', function() {
+            const localId = this.value;
+            const estoqueSelect = document.getElementById('estoque');
 
-                    // Faz a requisição para a rota que retorna os estoques
-                    fetch(url)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (data.success) {
-                                // Adiciona os novos <option> para os estoques
-                                data.estoques.forEach(estoque => {
-                                    const option = document.createElement('option');
-                                    option.value = estoque.id;
-                                    option.textContent = estoque.nome_estoque;
-                                    estoqueSelect.appendChild(option);
-                                });
-                            } else {
-                                console.error('Erro ao carregar estoques:', data.message);
-                            }
-                        })
-                        .catch(error => {
-                            console.error('Erro ao carregar estoques:', error);
-                        });
-                }
-            });
+            // Limpa os estoques existentes
+            estoqueSelect.innerHTML = '<option value="">Selecione</option>';
 
-            // Verifica se já existem valores totais ao carregar a lista de itens
-            if (document.querySelectorAll('[id^="total_"]').length > 0) {
-                atualizarTotalGeral(); // Atualiza o total geral ao carregar os itens
+            if (localId) {
+
+                const url = `/escolas/${localId}/estoques/getEstoques`;
+
+                // Faz a requisição para a rota que retorna os estoques
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            // Adiciona os novos <option> para os estoques
+                            data.estoques.forEach(estoque => {
+                                const option = document.createElement('option');
+                                option.value = estoque.id;
+                                option.textContent = estoque.nome_estoque;
+                                estoqueSelect.appendChild(option);
+                            });
+                        } else {
+                            console.error('Erro ao carregar estoques:', data.message);
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erro ao carregar estoques:', error);
+                    });
             }
         });
 
-        // Função para atualizar o total geral da tabela
-        function atualizarTotalGeral() {
-            let totalGeral = 0;
-
-            // Seleciona todas as células com id começando com "total_"
-            const totais = document.querySelectorAll('[id^="total_"]');
-
-            totais.forEach(total => {
-                let valor = parseFloat(total.innerText.replace('R$ ', '').replace(',', '.'));
-
-                // Soma o valor total
-                if (!isNaN(valor)) {
-                    totalGeral += valor;
-                }
-            });
-
-            // Atualiza o total geral no cabeçalho
-            document.getElementById('totalGeral').innerText = `R$ ${totalGeral.toFixed(2).replace('.', ',')}`;
+        // Verifica se já existem valores totais ao carregar a lista de itens
+        if (document.querySelectorAll('[id^="total_"]').length > 0) {
+            atualizarTotalGeral(); // Atualiza o total geral ao carregar os itens
         }
+    });
 
-        // Função para alterar a quantidade do pedido
-        function alterarQuantidade(estoqueProdutoId, incremento, preco) {
-            const quantidadeInput = document.getElementById('quantidade_' + estoqueProdutoId);
-            let quantidadeAtual = parseFloat(quantidadeInput.value);
+    // Função para atualizar o total geral da tabela
+    function atualizarTotalGeral() {
+        let totalGeral = 0;
 
-            if (isNaN(quantidadeAtual)) {
-                quantidadeAtual = 0;
+        // Seleciona todas as células com id começando com "total_"
+        const totais = document.querySelectorAll('[id^="total_"]');
+
+        totais.forEach(total => {
+            let valor = parseFloat(total.innerText.replace('R$ ', '').replace(',', '.'));
+
+            // Soma o valor total
+            if (!isNaN(valor)) {
+                totalGeral += valor;
             }
+        });
 
-            // Atualiza a quantidade com o incremento
-            quantidadeInput.value = Math.max(0, quantidadeAtual + incremento);
+        // Atualiza o total geral no cabeçalho
+        document.getElementById('totalGeral').innerText = `R$ ${totalGeral.toFixed(2).replace('.', ',')}`;
+    }
 
-            // Atualiza o total após a alteração
-            atualizarTotal(estoqueProdutoId, preco);
+    // Função para alterar a quantidade do pedido
+    function alterarQuantidade(estoqueProdutoId, incremento, preco) {
+        const quantidadeInput = document.getElementById('quantidade_' + estoqueProdutoId);
+        let quantidadeAtual = parseFloat(quantidadeInput.value);
+
+        if (isNaN(quantidadeAtual)) {
+            quantidadeAtual = 0;
         }
 
-        // Função para atualizar o total de um item específico
-        function atualizarTotal(estoqueProdutoId, preco) {
-            const quantidadeInput = document.getElementById('quantidade_' + estoqueProdutoId);
-            let quantidadeAtual = parseFloat(quantidadeInput.value);
+        // Atualiza a quantidade com o incremento
+        quantidadeInput.value = Math.max(0, quantidadeAtual + incremento);
 
-            // Verifica se a quantidade é negativa
-            if (isNaN(quantidadeAtual) || quantidadeAtual < 0) {
-                quantidadeAtual = 0; // Se for negativo, reseta para 0
-                quantidadeInput.value = 0; // Atualiza o input com o valor 0
-            }
+        // Atualiza o total após a alteração
+        atualizarTotal(estoqueProdutoId, preco);
+    }
 
-            // Calcula o total
-            const total = preco * quantidadeAtual;
+    // Função para atualizar o total de um item específico
+    function atualizarTotal(estoqueProdutoId, preco) {
+        const quantidadeInput = document.getElementById('quantidade_' + estoqueProdutoId);
+        let quantidadeAtual = parseFloat(quantidadeInput.value);
 
-            // Atualiza o total na tabela
-            document.getElementById('total_' + estoqueProdutoId).innerText = `R$ ${total.toFixed(2).replace('.', ',')}`;
-
-            // Atualiza o total geral após a mudança
-            atualizarTotalGeral();
+        // Verifica se a quantidade é negativa
+        if (isNaN(quantidadeAtual) || quantidadeAtual < 0) {
+            quantidadeAtual = 0; // Se for negativo, reseta para 0
+            quantidadeInput.value = 0; // Atualiza o input com o valor 0
         }
-    </script>
+
+        // Calcula o total
+        const total = preco * quantidadeAtual;
+
+        // Atualiza o total na tabela
+        document.getElementById('total_' + estoqueProdutoId).innerText = `R$ ${total.toFixed(2).replace('.', ',')}`;
+
+        // Atualiza o total geral após a mudança
+        atualizarTotalGeral();
+    }
+</script>
